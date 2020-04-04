5a73c0d1e9d9372be4ec6d791574db6f4e72dc1c
evertonProgramadorCriativo <evertonprogramadorcriativo@gmail.com>
2020-04-04 20:23:47 -0300

    admin - login  (teste para ver ser o usuario esta ok)

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ require_once("vendor/autoload.php");
 
 use \Slim\Slim;
 use \Hcode\Page;
+use \Hcode\PageAdmin;
+use \Hcode\Model\User;
+
 
 
 
@@ -18,6 +21,33 @@ $app->get('/', function() {
 
 	$page->setTpl("index");
  
+});
+
+$app->get('/admin', function() {
+	
+	$page = new PageAdmin();
+
+	$page->setTpl("index");
+ 
+});
+
+
+$app->get('/admin/login', function() {
+ 
+	$page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+
+	$page->setTpl("login");
+});
+
+$app->post('/admin/login', function() {
+
+	User::login($_POST["login"], $_POST["password"]);
+
+	header("Location: /admin");
+	exit;
 });
 
 $app->run();
---
+++ vendor/hcodebr/php-classes/src/Model/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Hcode\Model;
+
+use \Hcode\DB\Sql;
+use \Hcode\Model;
+
+class User extends Model {
+
+    public static function login($login, $password) {
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_users WHERE deslogin = :LOGIN", array(
+              ":LOGIN"=>$login
+        ));
+
+        if(count($results) === 0) {
+            throw new \Exception("Usuario inexistente ou senha invalida.");
+        }
+
+        $data = $results[0];
+
+        if(password_verify($password, $data["despassword"]) === true) {
+
+            $user = new User();
+              
+            $user->setiduser($data["iduser"]);
+
+            var_dump($user);
+            exit;
+
+
+        } else {
+            throw new  \Exception("Usuário  inexistente ou  senha inválida.");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+?>
